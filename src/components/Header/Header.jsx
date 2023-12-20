d2fe175007e98402c4fb1e7d04c3c9366059ffac
Haseri <lessskoff@gmail.com>
2023-12-20 13:22:20 +0300

    Developing...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,12 @@ import useTelegram from "../../hooks/useTelegram";
 import "./Header.css";
 
 const Header = () => {
-  const { user, onClose, onToggleButton } = useTelegram();
+  const { user, onClose } = useTelegram();
 
   return (
     <header className={"header"}>
       <Button onClick={onClose}>Закрыть</Button>
       <span className={"username"}>{user?.username}</span>
-      <Button onClick={onToggleButton}>Toggle</Button>
     </header>
   );
 };
